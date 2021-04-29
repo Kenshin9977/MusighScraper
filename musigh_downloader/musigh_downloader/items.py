d367b8b0eb39ab0709282033c4d447aa4559922b
Kenshin <kenshin9955@hotmail.fr>
2021-04-29 03:43:59 +0200

    Initial commit.

---
+++ musigh_downloader/musigh_downloader/items.py
@@ -0,0 +1,10 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MusighFileItem(scrapy.Item):
+    file_urls = scrapy.Field()
---
+++ musigh_downloader/musigh_downloader/spiders/musigh.py
@@ -0,0 +1,36 @@
+import os
+
+import scrapy
+
+# from musigh_downloader.musigh_downloader.items import MusighFileItem
+from musigh_downloader.items import MusighFileItem
+
+# from musigh_downloader.items import MusighImageItem
+
+
+class MusighSpider(scrapy.Spider):
+    name = 'musigh'
+    # allowed_domains = ['musigh.com']
+    start_urls = ['http://musigh.com/wp-content/uploads/']
+
+    def parse(self, response):
+        for i in range(4, response.xpath('//tr').__len__()):
+            yield scrapy.Request(
+                response.urljoin(response.xpath('//tr[' + str(i) + ']/td[2]/a').attrib['href']),
+                self.parse_month
+            )
+
+    def parse_month(self, response):
+        for i in range(4, response.xpath('//tr').__len__()):
+            yield scrapy.Request(
+                response.urljoin(response.xpath('//tr[' + str(i) + ']/td[2]/a').attrib['href']),
+                self.scrap
+            )
+
+    def scrap(self, response):
+        for i in range(4, response.xpath('//tr').__len__()):
+            file_url = response.urljoin(response.xpath('//tr[' + str(i) + ']/td[2]/a').attrib['href'])
+            yield MusighFileItem(file_urls=[file_url])
+
+
+
